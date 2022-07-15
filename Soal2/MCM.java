2d0bbce182fc8e3c1098833e455fc44080661caf
Justadude123 <malikdzaki16@gmail.com>
2022-07-15 13:45:05 +0700

    Update MCM.java

--- Soal2/MCM.java
+++ Soal2/MCM.java
@@ -2,12 +2,12 @@ package Soal2;
 
 // import java.util.*;
 
-class Matrix {
-    double[][] m;
+class MatrixMCM {
+    double[][] harga;
     int[][] s;
 
-    Matrix(double[][] m, int[][] s) {
-        this.m = m;
+    MatrixMCM(double[][] harga, int[][] s) {
+        this.harga = harga;
         this.s = s;
     }
 }
@@ -15,10 +15,10 @@ class Matrix {
 public class MCM {
 
     public static void main(String[] args) {
-        int[] p = { 30, 35, 15, 5, 10, 20, 25 };
-        Matrix a = MCMopt(p);
+        int[] p = { 30, 35, 15, 5, 10, 20, 25, 70, 20, 50, 40, 30, 35, 15, 30, 35, 15, };
+        MatrixMCM a = MCMopt(p);
         int[][] s = a.s;
-        // System.out.println(Arrays.deepToString(a.m));
+        // System.out.println(Arrays.deepToString(a.harga));
         // System.out.println(Arrays.deepToString(a.s));
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s[0].length; j++) {
@@ -26,29 +26,29 @@ public class MCM {
             }
             System.out.println();
         }
-        parens(a.s, 1, 5);
+        parens(a.s, 1, p.length - 1);
 
     }
 
-    public static Matrix MCMopt(int[] points) {
+    public static MatrixMCM MCMopt(int[] points) {
         int n = points.length;
         int p[] = points;
-        double[][] m = new double[n][n];
+        double[][] harga = new double[n][n];
         int[][] s = new int[n][n];
         n = points.length - 1;
 
         for (int i = 0; i <= n; i++) {
-            m[i][i] = 0;
+            harga[i][i] = 0;
         }
 
         for (int l = 2; l <= n; l++) {
             for (int i = 1; i <= n - l + 1; i++) {
                 int j = i + l - 1;
-                m[i][j] = Double.POSITIVE_INFINITY;
+                harga[i][j] = Double.POSITIVE_INFINITY;
                 for (int k = i; k <= j - 1; k++) {
-                    double q = m[i][k] + m[k + 1][j] + (p[i - 1] * p[k] * p[j]);
-                    if (q < m[i][j]) {
-                        m[i][j] = q;
+                    double q = harga[i][k] + harga[k + 1][j] + (p[i - 1] * p[k] * p[j]);
+                    if (q < harga[i][j]) {
+                        harga[i][j] = q;
                         s[i][j] = k;
                     }
                 }
@@ -56,7 +56,7 @@ public class MCM {
             }
         }
 
-        Matrix a = new Matrix(m, s);
+        MatrixMCM a = new MatrixMCM(harga, s);
 
         return a;
     }
