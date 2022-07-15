83ec9391122c7546ec4d81c0352cc72949fdac60
Justadude123 <malikdzaki16@gmail.com>
2022-07-15 14:13:50 +0700

    Update LexicographicOrderPermutations.java

--- Soal1/LexicographicOrderPermutations.java
+++ Soal1/LexicographicOrderPermutations.java
@@ -1,5 +1,22 @@
 package Soal1;
+
 public class LexicographicOrderPermutations {
     public static void main(String[] args) {
+    }
+
+    public static void getNextLexicographicOrderPermutation(int[] currPerm, int length) {
+        int i = length - 1;
+        while (i >= 0 && currPerm[i - 1] >= currPerm[i]) {
+            i--;
+        }
+
+        if (i < 0) {
+            return;
+        }
+
+        int j = length;
+        while (j > i && currPerm[j - 1] <= currPerm[i - 1]) {
+            j--;
+        }
     }
 }
