af15338bacecf6bc7e6f50e9706d7982e07b4ec0
Justadude123 <malikdzaki16@gmail.com>
2022-07-15 13:31:12 +0700

    FIXXXX

--- Soal2/LCS.java
+++ Soal2/LCS.java
@@ -2,67 +2,67 @@ package Soal2;
 
 import java.util.*;
 
-class MatrixA {
-    int[][] c;
-    char[][] b;
+class MatrixLCS {
+    int[][] harga;
+    char[][] arah;
 
-    MatrixA(int[][] c, char[][] b) {
-        this.c = c;
-        this.b = b;
+    MatrixLCS(int[][] harga, char[][] arah) {
+        this.harga = harga;
+        this.arah = arah;
     }
 
-    MatrixA() {
+    MatrixLCS() {
     }
 }
 
 public class LCS {
     public static void main(String[] args) {
-        String x = "ABCBDAB";
-        String y = "BDCABA";
-        char[] X = x.toCharArray();
-        char[] Y = y.toCharArray();
-        MatrixA a = LCSLength(X, Y);
-        char[][] b = a.b;
-        printLCS(b, X, X.length, Y.length);
-        System.out.println(Arrays.deepToString(b));
+        String string1 = "ABCBDAB";
+        String string2 = "BDCABA";
+        char[] charString1 = string1.toCharArray();
+        char[] charString2 = string2.toCharArray();
+        MatrixLCS a = panjangLCS(charString1, charString2);
+        char[][] arah = a.arah;
+        printLCS(arah, charString1, charString1.length, charString2.length);
+        System.out.println(Arrays.deepToString(arah));
     }
 
-    public static MatrixA LCSLength(char[] x, char[] y) {
-        int m = Math.max(x.length, y.length) + 1;
-        int n = Math.min(x.length, y.length) + 1;
-        int c[][] = new int[m][n];
-        char b[][] = new char[m][n];
+    public static MatrixLCS panjangLCS(char[] string1, char[] string2) {
+        int m = Math.max(string1.length, string2.length) + 1;
+        int n = Math.min(string1.length, string2.length) + 1;
+        int harga[][] = new int[m][n];
+        char arah[][] = new char[m][n];
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (x[i - 1] == y[j - 1]) {
-                    c[i][j] = c[i - 1][j - 1] + 1;
-                    b[i][j] = '\\';
-                } else if (c[i - 1][j] >= c[i][j - 1]) {
-                    c[i][j] = c[i - 1][j];
-                    b[i][j] = '|';
+                if (string1[i - 1] == string2[j - 1]) {
+                    harga[i][j] = harga[i - 1][j - 1] + 1;
+                    arah[i][j] = '\\';
+                } else if (harga[i - 1][j] >= harga[i][j - 1]) {
+                    harga[i][j] = harga[i - 1][j];
+                    arah[i][j] = '|';
                 } else {
-                    c[i][j] = c[i][j - 1];
-                    b[i][j] = '-';
+                    harga[i][j] = harga[i][j - 1];
+                    arah[i][j] = '-';
                 }
             }
         }
-        MatrixA a = new MatrixA(c, b);
+        MatrixLCS a = new MatrixLCS(harga, arah);
         return a;
     }
 
-    public static void printLCS(char[][] b, char[] X, int i, int j) {
+    public static void printLCS(char[][] arah, char[] charString1, int i, int j) {
         if (i == 0 || j == 0) {
             return;
         }
 
-        if (b[i][j] == '\\') {
-            printLCS(b, X, i - 1, j - 1);
-            System.out.println(X[i - 1]);
-        } else if (b[i][j] == '|') {
-            printLCS(b, X, i - 1, j);
+        if (arah[i][j] == '\\') {
+            printLCS(arah, charString1, i - 1, j - 1);
+            System.out.println(charString1[i - 1]);
+        } else if (arah[i][j] == '|') {
+            printLCS(arah, charString1, i - 1, j);
         } else {
-            printLCS(b, X, i, j - 1);
+            printLCS(arah, charString1, i, j - 1);
         }
     }
 
